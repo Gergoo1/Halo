60a5d059ed58be72cc5b692d630e89e0dbececdb
kohutg <91116409+Gergoo1@users.noreply.github.com>
2022-10-03 09:34:15 +0200

    Add files via upload

---
+++ 9-10. óra.py
@@ -0,0 +1,10 @@
+# első feladat 9 DK
+Idei_ev = 2022
+print(type(Idei_ev))
+print('az Idei_ev változó értéke: ', Idei_ev, sep='--->') 
+felhasznalo_kora = input("Hány éves vagy?")
+print(type(felhasznalo_kora))
+print('Te most', felhasznalo_kora, 'éves vagy.')
+
+szuletesi_ev = Idei_ev - felhasznalo_kora
+print('Ekkor születtél:', szuletesi_ev,'.', sep='')

34ad9bcae275781b1d23e4aa1aa54462c0bc95fa
kohutg <91116409+Gergoo1@users.noreply.github.com>
2022-10-10 09:40:47 +0200

    Add files via upload

---
+++ 11-12. óra.py
@@ -0,0 +1,22 @@
+gondolt_szám = 4
+print(type(11))
+tipp = input('Gondoltam egy számra. Tippeld meg! ')
+tipp = int(tipp)
+tipp == gondoltam_szám:
+print('ügyes!)
+if tipp > gondolt_szám:
+    print('A tipp nagyobb mint a gondolt szám')
+if tipp < gondolt_szám:
+    print('A tipp nagyobb mint a gondolt szám')
+
+print= ('pápá')
+
+
+gondolt_szám = 4
+tipp = input('')
+tipp int(tipp)
+if tipp = gondolt_szám:
+    print('Ügyes!')
+else:
+    print('Nem találtad el')
+print= ('pápá')

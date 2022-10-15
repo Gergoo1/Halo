2562e82156d9941c17d9f9085b666d944e1b84ab
kohutg <91116409+Gergoo1@users.noreply.github.com>
2022-10-15 09:14:31 +0200

    Add files via upload

---
+++ 13.-14. ora.py
@@ -0,0 +1,16 @@
+# 1. feladat
+# Kérjünk be két számot (egész szám) irassuk ki a nagyobb számot
+
+első_szám=int(input("Kérek egy egész számot: "))
+második_szám=int(input("Kérek még egy számot: "))
+'''
+if első_szám > második szám:
+  print('az első szám nagyobb', első_szám)
+if második_szám > első_szám:
+  print('a második szám nagyobb',második_szám)
+'''
+if egyikszam > masikszam:
+   print(egyikszam)
+elif masikszam > masikszam:
+   print(masikszam)
+else: print('az egyik szam = a masikszammal')

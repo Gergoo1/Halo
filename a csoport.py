b715e472ee82c9b6ef078dbc177019fc91c9f6ab
kohutg <91116409+Gergoo1@users.noreply.github.com>
2022-11-28 09:42:07 +0100

    Add files via upload

---
+++ a csoport.py
@@ -0,0 +1,31 @@
+"""
+for szam in range (50,101,3):
+    print(szam, end=" ")
+
+
+for szam in range (50,100,3,):
+     print(szam, end=" ")
+     print()
+for szam in range (50,101,3):
+    if szam % 2 == 0:
+    print(szam,end=" ")
+print("program vége")
+
+
+szoveg="Hello világ!"
+for kar in szoveg:
+    print(kar, end="")
+
+szoveg = input("Kérem a nevedet:")
+for kar in szoveg:
+    print(kar.upper (), end="") 
+"""
+szamlalo=0
+nev = input("Kérem a nevedet:") #legyen neved = kérem a teljes nevedet kisbetűvel
+for kar in szoveg:
+if szamlalo ==0:
+print(kar, end="") 
+   print(kar.upper, end="")
+   else:  
+    #print(szamlalo, end=" ")
+    szamlalo += 1
